1eff9360ac23ad63dc69948f533303f5bad6d37f
Dork <imnotaprogrammer@protonmail.com>
2020-09-11 22:41:19 -0500

    Migrate views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_004434) do
+ActiveRecord::Schema.define(version: 2020_09_12_033637) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_09_08_004434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id"
+    t.integer "views", default: 0
     t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
