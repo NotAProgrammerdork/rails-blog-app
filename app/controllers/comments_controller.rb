d01f0f41f839d2bbb403a3e68c2db16c1d6842d4
Dork <imnotaprogrammer@protonmail.com>
2020-09-11 21:58:45 -0500

    Allow the deletion of comments for registered users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,15 @@ class CommentsController < ApplicationController
     redirect_to article_path(@comment.article)
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to article_path(params[:article_id])
+  end
+
+  private
+
   def comment_params
     params.require(:comment).permit(:author_name, :body)
   end
